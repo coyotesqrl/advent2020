b1feb73afa4c97ffad384a6782e67869ad2fef55
R.A. Porter <richard.porter@connexta.com>
2020-12-02 08:33:11 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Advent of Code 2020
+Hacked implementations, naturally. Use repl for invoking each puzzle's solutions.
+Comment has invocation examples.
+
+To run tests, run `clj -M:test` at the command line.
---
+++ src/advent2020.clj
@@ -0,0 +1,96 @@
+(ns advent2020
+  (:require [clojure.math.combinatorics :as comb]
+            [clojure.test :refer :all]
+            [clojure.java.io :as io]))
+
+(defn read-input
+  [filename]
+  (line-seq (io/reader (io/resource filename))))
+
+(defn extract-addends
+  [num-addends exp-sum input]
+  (->> (comb/combinations input num-addends)
+       (filter #(= exp-sum (apply + %)))
+       first))
+
+(defn count-chars
+  [c s]
+  (->> (char-array s)
+       (filter #{c})
+       count))
+
+(defn password-rule-match-1
+  [[min max ltr pwd]]
+  (let [cnt (count-chars ltr pwd)]
+    (and (<= cnt max) (>= cnt min))
+    ))
+
+(defn password-rule-match-2
+  [[idx1 idx2 ltr pwd]]
+  (let [pwd-arr (char-array pwd)
+        ch1 (get pwd-arr (- idx1 1))
+        ch2 (get pwd-arr (- idx2 1))]
+    (or
+      (and
+        (= ch1 ltr) (not= ch2 ltr))
+      (and
+        (= ch2 ltr) (not= ch1 ltr)))))
+
+(defn read-password-line
+  "Reads password line from db and returns min, max, ltr, pwd"
+  [line]
+  (let [mtc (re-matches #"(\d*)-(\d*)\s*(.):\s*(.*)$" line)]
+    (list (Integer/parseInt (second mtc))
+          (Integer/parseInt (nth mtc 2))
+          (first (char-array (nth mtc 3)))
+          (nth mtc 4))))
+
+;
+; Main entry functions below this line
+;
+(defn advent-1
+  "Extract `num-addends` addends from expense report list that together sum to 2020, then
+  multiplies them together."
+  [num-addends input]
+  (->> (read-input input)
+       (map #(Integer/parseInt %))
+       (extract-addends num-addends 2020)
+       (apply *)))
+
+(defn advent-2
+  "Parses each line of the input and counts passwords that are valid given the `pw-match-fn`
+  rule function."
+  [pw-match-fn input]
+  (->> (read-input input)
+       (filter #(pw-match-fn (read-password-line %)))
+       count))
+
+(comment
+  (advent-1 2 "day1.txt")
+  (advent-1 3 "day1.txt")
+  (advent-2 password-rule-match-1 "day2.txt")
+  (advent-2 password-rule-match-2 "day2.txt")
+  )
+
+;
+; Tests below this line
+;
+(defn -main []
+  (run-tests 'advent2020))
+
+(deftest test-password-parse
+  (is (= '(1 3 \a "abcde") (read-password-line "1-3 a: abcde"))))
+
+(deftest test-password-rule-matchers
+  (are [matchfn pw valid] (= (matchfn pw) valid)
+                          password-rule-match-1 '(1 3 \a "defaghi") true
+                          password-rule-match-1 '(1 3 \a "defaghia") true
+                          password-rule-match-1 '(1 3 \a "defaghiaxda") true
+                          password-rule-match-1 '(1 3 \a "defaghiacaca") false
+                          password-rule-match-2 '(3 6 \b "axbhkklaid") true
+                          password-rule-match-2 '(3 6 \b "axb") true
+                          password-rule-match-2 '(3 6 \b "xyakeb") true
+                          password-rule-match-2 '(3 6 \b "xyakebbbb") true
+                          password-rule-match-2 '(3 6 \b "xybkebbbb") false
+                          password-rule-match-2 '(3 6 \b "xyckecccc") false
+                          password-rule-match-2 '(3 6 \b "xyckecd") false))
